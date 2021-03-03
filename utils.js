01ef5b8f380697cd02e8b7e6eecfdd6ead502706
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-03 15:22:28 -0800

    added a subtract function to utils.js

--- utils.js
+++ utils.js
@@ -10,6 +10,11 @@ const power = (a, b) => {
     return Math.pow(a , b);
 }
 
+const subtract = (a, b) => {
+    return a - b;
+};
+
 console.log(product(8,4));
 console.log(divide(8,4));
 console.log(power(8,4));
+console.log(subtract(8,4));
