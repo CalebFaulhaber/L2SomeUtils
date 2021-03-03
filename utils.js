eaa7cbe869c612f7a196a010307c592a26516c29
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-03-03 15:10:17 -0800

    created these all before had to delete, so doing balk commenting

---
+++ utils.js
@@ -0,0 +1,15 @@
+const product = (a, b) => {
+    return (a * b);
+};
+
+const divide = (a, b) => {
+    return (a / b);
+};
+
+const power = (a, b) => {
+    return Math.pow(a , b);
+}
+
+console.log(product(8,4));
+console.log(divide(8,4));
+console.log(power(8,4));
